c72275f7fe55058d50499fdc5acf03e302686fa9
Hermann227 <31062415+Hermann227@users.noreply.github.com>
2017-08-16 13:13:41 +0200

    Add files via upload

--- default.php
+++ default.php
@@ -3,25 +3,20 @@
 
 //Minimum request
 //Can be city,state,country, zip/postal code, IP address, longtitude/latitude. If long/lat are 2 elements, they will be assembled. IP address is one element.
-$loc_array= Array("New York","ny");		//data validated in foreach. 
+$loc="Cologne";		//data validated in foreach. 
 $api_key="b43261f1ced54ae6b1e95314171608";		//should be embedded in your code, so no data validation necessary, otherwise if(strlen($api_key)!=24)
 $num_of_days=1;					//data validated in sprintf
-
-$loc_safe=Array();
-foreach($loc_array as $loc){
-	$loc_safe[]= urlencode($loc);
-}
-$loc_string=implode(",", $loc_safe);
+$date="2017-08-16";
 
 //To add more conditions to the query, just lengthen the url string
-$basicurl=sprintf('http://api.worldweatheronline.com/free/v1/weather.ashx?key=%s&q=%s&num_of_days=%s&format=json', 
-	$api_key, $loc_string, intval($num_of_days));
+$basicurl=sprintf('http://api.worldweatheronline.com/free/v1/weather.ashx?key=%s&q=%s&date=%s&format=json', 
+	$api_key, $loc, $date);
 
 print $basicurl . "<br />\n";
 
 //Premium API
-$premiumurl=sprintf('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=%s&q=%s&num_of_days=%s&format=json', 
-	$api_key, $loc_string, intval($num_of_days));
+$premiumurl=sprintf('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=%s&q=%s&date=%s&format=json', 
+	$api_key, $loc, $date);
 	
 print $premiumurl . "<br />\n";
 
@@ -32,10 +27,11 @@ print_r($json_reply);
 print "</pre>";
 
 $json=json_decode($json_reply);
-printf("<p>Current wind speed is %s mph blowing to %s</p>", 
+printf("<p>Die Temperatur in %s am %s beträgt %s Grad Celsius.</p>", 
 	//~ $json->{'data'}->{'current_condition'}->{'windspeedMiles'}, $json->{'data'}->{'current_condition'}->{'winddir16Point'} );
-	$json->{'data'}->{'current_condition'}['0']->{'windspeedMiles'}, 
-	$json->{'data'}->{'current_condition'}['0']->{'winddir16Point'} );
+	$json->{'data'}->{'request'}['0']->{'query'}, 
+	$json->{'data'}->{'weather'}['0']->{'date'}, 
+	$json->{'data'}->{'current_condition'}['0']->{'temp_C'} );
 //~ print "<script>var weather= JSON.parse(";
 //~ print $json_reply;
 //~ print ");</script>";
