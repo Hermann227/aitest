0d38371a9708292a314dfc44b017201672caf6d2
Hermann227 <31062415+Hermann227@users.noreply.github.com>
2017-08-16 17:33:44 +0200

    Add files via upload

--- translate.php
+++ translate.php
@@ -1,31 +1,40 @@
 <?php
-$reqText = urlencode("Heute ist Mittwoch und das Wetter ist herrlich");
+$method = $_SERVER['REQUEST_METHOD'];
 
-print $reqText;
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
 
-$premiumurl = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" . $reqText;
+	$sourceLanguage = $json->result->parameters->lang-from;
+	$sourceLanguage = substr($sourceLanguage, 0, 2);
+	$targetLanguage = $json->result->parameters->lang-to;
+	$targetLanguage = substr($targetLanguage, 0, 2);
+	$reqText = $json->result->resolvedQuery;
+	$reqText = urlencode(reqText);
+	
+	$premiumurl = sprintf('https://translate.googleapis.com/translate_a/single?client=gtx&sl=%s&tl=%s&dt=t&q=%s', 
+		$sourceLanguage, $targetLanguage, $reqText);
 
-$json_reply = file_get_contents($premiumurl);
+	$json_reply = file_get_contents($premiumurl);
 
-print "<pre>";
-print_r($json_reply);
-print "</pre>";
-
-$json=json_decode($json_reply);
-
-print "<pre>";
-print_r($json);
-print "</pre>";
-
-$sourceText = $json[0][0][1];
-//$sourceText = substr_replace($sourceText ,"",-1);
-
-$translatedText = $json[0][0][0];
-//$translatedText = substr_replace($translatedText ,"",-1);
-
-$speech = sprintf("Der Text \"%s\" heißt übersetzt \"%s.\"", 
+	$json=json_decode($json_reply);
+	
+	$sourceText = $json[0][0][1];
+	$translatedText = $json[0][0][0];
+	
+	$speech = sprintf("Der Text \"%s\" heißt übersetzt \"%s.\"", 
 		$sourceText, 
 		$translatedText);
 
-print $speech;
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
 ?> 
