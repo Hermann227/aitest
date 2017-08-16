5528f37f617ce8ad06512638ebc03e6a0d955ced
Hermann227 <31062415+Hermann227@users.noreply.github.com>
2017-08-16 13:49:31 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
@@ -7,25 +6,24 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->resolvedQuery;
+	$loc = $json->result->parameters->address->subadmin-area;
+	$date = $json->result->parameters->date-time;
+	
+	//Can be city,state,country, zip/postal code, IP address, longtitude/latitude. If long/lat are 2 elements, they will be assembled. IP address is one element.
+	$api_key="b43261f1ced54ae6b1e95314171608";		//should be embedded in your code, so no data validation necessary, otherwise if(strlen($api_key)!=24)
 
-	switch ($text) {
-		case 'Wetter':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'Wettervorhersage ':
-			$speech = "Bye, good night";
-			break;
-
-		case 'Wetterbericht':
-			$speech = "Yes, you can type anything here.";
-			break;
+	//Premium API
+	$premiumurl=sprintf('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=%s&q=%s&date=%s&format=json', 
+		$api_key, $loc, $date);
 		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-	}
+	$json_reply = file_get_contents($premiumurl);
+
+	$json=json_decode($json_reply);
+	
+	$speech = sprintf("Die Temperatur in %s am %s beträgt %s Grad Celsius.", 
+		$json->{'data'}->{'request'}['0']->{'query'}, 
+		$json->{'data'}->{'weather'}['0']->{'date'}, 
+		$json->{'data'}->{'current_condition'}['0']->{'temp_C'} );
 
 	$response = new \stdClass();
 	$response->speech = $speech;
@@ -38,4 +36,5 @@ else
 	echo "Method not allowed";
 }
 
-?>
+
+?> 
