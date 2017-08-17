2ca3c22d45bd2ec09af38b098d68db90f72132a6
Hermann227 <31062415+Hermann227@users.noreply.github.com>
2017-08-17 15:27:49 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -17,8 +17,7 @@ if($method == 'POST'){
 			$api_key="b43261f1ced54ae6b1e95314171608";		//should be embedded in your code, so no data validation necessary, otherwise if(strlen($api_key)!=24)
 
 			//Premium API
-			$premiumurl=sprintf('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=%s&q=%s&date=%s&format=json', 
-				$api_key, $loc, $datum );
+			$premiumurl=sprintf('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=%s&q=%s&date=%s&format=json', $api_key, $loc, $datum );
 				
 			$json_reply = file_get_contents($premiumurl);
 
@@ -29,10 +28,7 @@ if($method == 'POST'){
 			//$date = new DateTime($originalDate);
 			//$newDate = $date->format('d.m.Y');
 			
-			$speech = sprintf("Die Temperatur in %s am %s beträgt %s Grad Celsius." . $premiumurl, 
-				$json->{'data'}->{'request'}['0']->{'query'}, 
-				$newDate, 
-				$json->{'data'}->{'current_condition'}['0']->{'temp_C'} );
+			$speech = sprintf("Die Temperatur in %s am %s beträgt %s Grad Celsius.", $json->{'data'}->{'request'}['0']->{'query'}, $newDate, $json->{'data'}->{'current_condition'}['0']->{'temp_C'} );
 			break;
 
 		case 'translate.text':
@@ -52,9 +48,7 @@ if($method == 'POST'){
 			$sourceText = $json[0][0][1];
 			$translatedText = $json[0][0][0];
 			
-			$speech = sprintf("Der Text \"%s\" heißt übersetzt \"%s\"." . $premiumurl, 
-				$sourceText, 
-				$translatedText );
+			$speech = sprintf("Der Text \"%s\" heißt übersetzt \"%s\".", $sourceText, $translatedText );
 			break;
 
 		default:
